a317112e4a6ccc2283b792f03753bc977a744158
Melina Boedecker <melinamboedecker@gmail.com>
2021-06-10 12:18:44 -0700

    removed console  logs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,21 +21,16 @@ module.exports = (app) => {
         note.id = uniqid();
         const database = JSON.parse(fs.readFileSync("./db/db.json/"));
         database.push(note);
-        console.log(database);
         fs.writeFileSync('./db/db.json', JSON.stringify(database));
         res.json ("your note has been saved")
     });
 
     app.delete('/api/notes/:id', (req, res) => {
-        console.log('dELETE');
-        console.log(req.params.id);
         const deleId = req.params.id;
         const dataToDele = JSON.parse(fs.readFileSync("./db/db.json/"));
-        console.log(dataToDele);
         let filtered = dataToDele.filter(function (el) {
             return el.id != deleId;
         });
-        console.log(filtered);
         fs.writeFileSync('./db/db.json', JSON.stringify(filtered));
         res.json ("your note has been deleted")
     })
