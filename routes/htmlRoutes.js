e3cde79b2a440cde528252838b62f243980803e9
Melina Boedecker <melinamboedecker@gmail.com>
2021-06-10 13:55:25 -0700

    changed get /  route to see if it works in heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,8 @@ module.exports = (app) => {
     // });
 
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+        // res.sendFile(path.join(__dirname, 'public/index.html'));
+        res.json(path.join(__dirname, 'public/index.html'));
     });
 
     app.get('/notes', (req, res) => {
