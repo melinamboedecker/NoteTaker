68f7b8a186fcb5fca120b5fd4ddc8489ad1f394f
Melina Boedecker <melinamboedecker@gmail.com>
2021-06-12 09:47:51 -0700

    adjusted paths in apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,19 +19,19 @@ module.exports = (app) => {
     app.post('/api/notes', (req, res) => {
         const note = req.body;
         note.id = uniqid();
-        const database = JSON.parse(fs.readFileSync("./db/db.json/"));
+        const database = JSON.parse(fs.readFileSync("../db/db.json/"));
         database.push(note);
-        fs.writeFileSync('./db/db.json', JSON.stringify(database));
+        fs.writeFileSync('../db/db.json', JSON.stringify(database));
         res.json ("your note has been saved")
     });
 
     app.delete('/api/notes/:id', (req, res) => {
         const deleId = req.params.id;
-        const dataToDele = JSON.parse(fs.readFileSync("./db/db.json/"));
+        const dataToDele = JSON.parse(fs.readFileSync("../db/db.json/"));
         let filtered = dataToDele.filter(function (el) {
             return el.id != deleId;
         });
-        fs.writeFileSync('./db/db.json', JSON.stringify(filtered));
+        fs.writeFileSync('../db/db.json', JSON.stringify(filtered));
         res.json ("your note has been deleted")
     })
 
