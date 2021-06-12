0f905549a999c877b0a73cd5609444f37d06d42a
Melina Boedecker <melinamboedecker@gmail.com>
2021-06-12 14:46:57 -0700

    test

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
-
+console.log("+++++++++++++++++++++++++++++++++++++++")
+console.log(__dirname);
+console.log("+++++++++++++++++++++++++++++++++")
 
 
 require(path.join(__dirname, './routes/apiroutes'))(app);
 require('./routes/htmlroutes')(app);
+
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
