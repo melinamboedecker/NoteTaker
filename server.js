671746b4390352aeb87293018a53a8b0948fbead
Melina Boedecker <melinamboedecker@gmail.com>
2021-06-12 07:52:27 -0700

    used path.join for readFile in apiroutes

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require('./routes/apiroutes')(app);
+require('./routes/htmlroutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
