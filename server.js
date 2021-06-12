335186378bca8711fab0f5f587442c6aad82f91a
Melina Boedecker <melinamboedecker@gmail.com>
2021-06-12 14:54:25 -0700

    removed static

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 console.log("+++++++++++++++++++++++++++++++++++++++")
 console.log(__dirname);
 console.log("+++++++++++++++++++++++++++++++++")
